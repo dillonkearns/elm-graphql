19d84c6509bd187d7675d768187b7102bcdd9644
Dillon Kearns <dillon@dillonkearns.com>
2017-11-22 05:55:28 -0800

    Init elm app.

---
+++ src/Main.elm
@@ -0,0 +1,55 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, h1, img)
+import Html.Attributes exposing (src)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.svg" ] []
+        , h1 [] [ text "Your Elm App is working!" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = init
+        , update = update
+        , subscriptions = always Sub.none
+        }
