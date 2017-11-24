a2f67a2c34cad0c8cf2c29c68240c1c8f3830d4d
Dillon Kearns <dillon@dillonkearns.com>
2017-11-24 08:04:09 -0800

    Remove comment.

--- src/Schema/Query.elm
+++ src/Schema/Query.elm
@@ -5,9 +5,6 @@ module Schema.Query exposing (..)
 import GraphqElm.Field as Field exposing (Field, FieldDecoder)
 
 
--- how to decode a MenuItem
-
-
 menuItems : FieldDecoder menuItem -> FieldDecoder (List menuItem)
 menuItems fieldDecoder =
     Field.listAt [ "data", "menuItems" ] fieldDecoder
