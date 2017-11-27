9f3ef86e58e07a9c1290dec1927556971036f400
Dillon Kearns <dillon@dillonkearns.com>
2017-11-26 16:00:43 -0800

    Test generating single item not list.

--- tests/GeneratorTests.elm
+++ tests/GeneratorTests.elm
@@ -9,21 +9,22 @@ import Test exposing (..)
 all : Test
 all =
     describe "generator"
-        [ test "parse non-object types" <|
+        [ test "simple string" <|
             \() ->
-                [ { name = "me", typeOf = Type.Scalar Type.NonNullable Scalar.String } ]
+                { name = "me", typeOf = Type.Scalar Type.NonNullable Scalar.String }
                     |> thing
                     |> Expect.equal
-                        """module Schema.Query exposing (..)
-
-import GraphqElm.Argument as Argument exposing (Argument)
-import GraphqElm.Field as Field exposing (Field, FieldDecoder)
-import GraphqElm.Object as Object exposing (Object)
-import GraphqElm.TypeLock exposing (TypeLocked(TypeLocked))
-import Json.Decode as Decode exposing (Decoder)
-
-
-me : Field.RootQuery String
+                        """me : Field.RootQuery String
+me =
+    Field.custom "me" Decode.string
+        |> Field.rootQuery
+"""
+        , test "list" <|
+            \() ->
+                { name = "captains", typeOf = Type.List Type.NonNullable (Type.Scalar Type.NonNullable Scalar.String) }
+                    |> thing
+                    |> Expect.equal
+                        """me : Field.RootQuery String
 me =
     Field.custom "me" Decode.string
         |> Field.rootQuery
@@ -31,9 +32,9 @@ me =
         ]
 
 
-thing : List { name : String, typeOf : Type } -> String
+thing : { name : String, typeOf : Type } -> String
 thing something =
-    prepend ++ (List.map generate something |> String.join "\n\n")
+    generate something
 
 
 prepend : String
