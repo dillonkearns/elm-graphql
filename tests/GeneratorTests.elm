3db04de8e871e73092d39fbf3342994e65c1c4ff
Dillon Kearns <dillon@dillonkearns.com>
2017-11-26 16:13:35 -0800

    Use string format library to interpolate.

--- tests/GeneratorTests.elm
+++ tests/GeneratorTests.elm
@@ -3,6 +3,7 @@ module GeneratorTests exposing (all)
 import Expect
 import GraphqElm.Parser.Scalar as Scalar exposing (Scalar)
 import GraphqElm.Parser.Type as Type exposing (Type)
+import String.Format
 import Test exposing (..)
 
 
@@ -12,16 +13,6 @@ all =
         [ test "simple string" <|
             \() ->
                 { name = "me", typeOf = Type.Scalar Type.NonNullable Scalar.String }
-                    |> thing
-                    |> Expect.equal
-                        """me : Field.RootQuery String
-me =
-    Field.custom "me" Decode.string
-        |> Field.rootQuery
-"""
-        , test "list" <|
-            \() ->
-                { name = "captains", typeOf = Type.List Type.NonNullable (Type.Scalar Type.NonNullable Scalar.String) }
                     |> thing
                     |> Expect.equal
                         """me : Field.RootQuery String
@@ -51,10 +42,45 @@ import Json.Decode as Decode exposing (Decoder)
 """
 
 
+
+-- generate : { name : String, typeOf : Type } -> String
+-- generate { name, typeOf } =
+--     """me : Field.RootQuery String
+-- me =
+--     Field.custom "me" Decode.string
+--         |> Field.rootQuery
+-- """
+
+
 generate : { name : String, typeOf : Type } -> String
 generate { name, typeOf } =
-    """me : Field.RootQuery String
-me =
-    Field.custom "me" Decode.string
+    String.Format.format3 """{1} : Field.RootQuery {2}
+{1} =
+    Field.custom "{1}" {3}
         |> Field.rootQuery
-"""
+""" ( name, "String", "Decode.string" )
+
+
+
+-- generate : { name : String, typeOf : Type } -> String
+-- generate { name, typeOf } =
+--     let
+--         decoder =
+--             "Decode.string"
+--
+--         decodesTo =
+--             "String"
+--     in
+--     name
+--         ++ " : Field.RootQuery "
+--         ++ decodesTo
+--         ++ "\n"
+--         ++ name
+--         ++ """ =
+--     Field.custom """
+--         ++ "\""
+--         ++ name
+--         ++ "\" "
+--         ++ decoder
+--         ++ "\n        |> Field.rootQuery\n"
+--
