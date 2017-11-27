851a31eb23e5c538b17e22812e7a5401fe9cb29f
Dillon Kearns <dillon@dillonkearns.com>
2017-11-27 07:01:16 -0800

    Remove unused boilerplate.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './main.css';
-import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
-
-Main.embed(document.getElementById('root'));
-
-registerServiceWorker();
