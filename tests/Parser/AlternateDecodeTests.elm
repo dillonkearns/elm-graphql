c28887679562d7e6dd2334a822e85a75b705985a
Dillon Kearns <dillon@dillonkearns.com>
2017-11-29 15:44:47 -0800

    Add alternate object decoder.

--- tests/Parser/AlternateDecodeTests.elm
+++ tests/Parser/AlternateDecodeTests.elm
@@ -1,7 +1,8 @@
 module Parser.AlternateDecodeTests exposing (all)
 
 import Expect
-import GraphqElm.Parser.Type as Type
+import GraphqElm.Parser.Scalar as Scalar
+import GraphqElm.Parser.Type as Type exposing (..)
 import Json.Decode as Decode exposing (Decoder)
 import Test exposing (Test, describe, test)
 
@@ -40,11 +41,104 @@ all =
                         (Ok
                             (Type.TypeDefinition "Weather" (Type.EnumType [ "CLOUDY", "SUNNY" ]))
                         )
+        , test "decodes object" <|
+            \() ->
+                """
+                        {
+          "possibleTypes": null,
+          "name": "MenuItem",
+          "kind": "OBJECT",
+          "interfaces": [],
+          "inputFields": null,
+          "fields": [
+            {
+              "type": {
+                "ofType": {
+                  "ofType": null,
+                  "name": "String",
+                  "kind": "SCALAR"
+                },
+                "name": null,
+                "kind": "NON_NULL"
+              },
+              "name": "description",
+              "isDeprecated": false,
+              "description": null,
+              "deprecationReason": null,
+              "args": []
+            },
+            {
+              "type": {
+                "ofType": {
+                  "ofType": null,
+                  "name": "ID",
+                  "kind": "SCALAR"
+                },
+                "name": null,
+                "kind": "NON_NULL"
+              },
+              "name": "id",
+              "isDeprecated": false,
+              "description": null,
+              "deprecationReason": null,
+              "args": []
+            },
+            {
+              "type": {
+                "ofType": {
+                  "ofType": null,
+                  "name": "String",
+                  "kind": "SCALAR"
+                },
+                "name": null,
+                "kind": "NON_NULL"
+              },
+              "name": "name",
+              "isDeprecated": false,
+              "description": null,
+              "deprecationReason": null,
+              "args": []
+            }
+          ],
+          "enumValues": null
+        }
+                        """
+                    |> Decode.decodeString decoder
+                    |> Expect.equal
+                        (Ok
+                            (TypeDefinition "MenuItem"
+                                (ObjectType
+                                    [ { name = "description", typeRef = TypeReference (Scalar Scalar.String) NonNullable }
+                                    , { name = "id", typeRef = TypeReference (Scalar Scalar.ID) NonNullable }
+                                    , { name = "name", typeRef = TypeReference (Scalar Scalar.String) NonNullable }
+                                    ]
+                                )
+                            )
+                        )
         ]
 
 
 decoder : Decoder Type.TypeDefinition
 decoder =
+    Decode.oneOf
+        [ enumDecoder
+        , objectDecoder
+        ]
+
+
+objectDecoder : Decoder Type.TypeDefinition
+objectDecoder =
+    Decode.map2 createObject
+        (Decode.field "name" Decode.string)
+        (Type.fieldDecoder
+            |> Decode.map (\{ name, ofType } -> { name = name, typeRef = Type.parseRef ofType })
+            |> Decode.list
+            |> Decode.field "fields"
+        )
+
+
+enumDecoder : Decoder Type.TypeDefinition
+enumDecoder =
     Decode.map2 createEnum
         (Decode.field "name" Decode.string)
         (Decode.string
@@ -57,3 +151,8 @@ decoder =
 createEnum : String -> List String -> Type.TypeDefinition
 createEnum enumName enumValues =
     Type.TypeDefinition enumName (Type.EnumType enumValues)
+
+
+createObject : String -> List Type.Field -> Type.TypeDefinition
+createObject objectName fields =
+    Type.TypeDefinition objectName (Type.ObjectType fields)
