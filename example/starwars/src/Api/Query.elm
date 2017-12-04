61a9f11b78b4122ed7733e294a82bd43a68a502a
Dillon Kearns <dillon@dillonkearns.com>
2017-12-04 14:23:45 -0800

    Remove unnecessary imports.

--- example/starwars/src/Api/Query.elm
+++ example/starwars/src/Api/Query.elm
@@ -1,14 +1,10 @@
 module Api.Query exposing (..)
 
 import Api.Object
-import Api.Object.Character
-import Api.Object.Droid
-import Api.Object.Human
 import Graphqelm.Argument as Argument exposing (Argument)
 import Graphqelm.Field as Field exposing (Field, FieldDecoder)
 import Graphqelm.Object as Object exposing (Object)
 import Graphqelm.Query as Query
-import Json.Decode as Decode exposing (Decoder)
 
 
 hero : Object hero Api.Object.Character -> Field.Query hero
