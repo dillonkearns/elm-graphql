3d860b38aa044b164aa314928f63e329ac6ea295
Dillon Kearns <dillon@dillonkearns.com>
2017-12-04 14:42:12 -0800

    Restore accidentally removed manually generated code example.

--- example/starwars/src/Api/Query.elm
+++ example/starwars/src/Api/Query.elm
@@ -1,15 +1,25 @@
 module Api.Query exposing (..)
 
+import Api.Enum.Episode
 import Api.Object
 import Graphqelm.Argument as Argument exposing (Argument)
 import Graphqelm.Field as Field exposing (Field, FieldDecoder)
 import Graphqelm.Object as Object exposing (Object)
 import Graphqelm.Query as Query
+import Json.Decode as Decode exposing (Decoder)
 
 
-hero : Object hero Api.Object.Character -> Field.Query hero
-hero object =
-    Object.single "hero" [] object
+hero : ({ episode : Maybe Api.Enum.Episode.Episode } -> { episode : Maybe Api.Enum.Episode.Episode }) -> Object hero Api.Object.Character -> Field.Query hero
+hero fillInArgs object =
+    let
+        optionalArgsThing =
+            fillInArgs { episode = Nothing }
+
+        optionalArgs =
+            [ Maybe.map (\value -> Argument.enum "episode" (toString value)) optionalArgsThing.episode ]
+                |> List.filterMap identity
+    in
+    Object.single "hero" optionalArgs object
         |> Query.rootQuery
 
 
