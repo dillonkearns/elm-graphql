88196d861949ba204f114b82a601487635bb344d
Dillon Kearns <dillon@dillonkearns.com>
2017-12-04 20:48:16 -0800

    Add map2 function.

--- src/Graphqelm.elm
+++ src/Graphqelm.elm
@@ -1,6 +1,21 @@
-module Graphqelm exposing (noOptionalArgs)
+module Graphqelm exposing (map2, noOptionalArgs)
+
+import Graphqelm.Field
+import Graphqelm.Object as Object exposing (Object)
 
 
 noOptionalArgs : a -> a
 noOptionalArgs =
     identity
+
+
+map2 :
+    (placeholder -> Object (a -> b -> result) typeLock)
+    -> placeholder
+    -> Graphqelm.Field.FieldDecoder a typeLock
+    -> Graphqelm.Field.FieldDecoder b typeLock
+    -> Object result typeLock
+map2 build buildTo a b =
+    build buildTo
+        |> Object.with a
+        |> Object.with b
