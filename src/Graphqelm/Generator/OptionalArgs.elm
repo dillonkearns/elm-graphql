d0089a2b30fd134cb84d8a898889b16cb9fccbcb
Dillon Kearns <dillon@dillonkearns.com>
2017-12-07 16:55:54 -0800

    Add OptionalArgs module.

--- src/Graphqelm/Generator/OptionalArgs.elm
+++ src/Graphqelm/Generator/OptionalArgs.elm
@@ -4,9 +4,36 @@ import Graphqelm.Parser.Type as Type
 
 
 type alias Result =
-    String
+    { annotatedArg : { annotation : String, arg : String }
+    , letBindings : List String
+    }
 
 
 generate : List Type.Arg -> Maybe Result
 generate args =
-    Nothing
+    case List.filterMap generateSingle args of
+        [] ->
+            Nothing
+
+        _ ->
+            Just
+                { annotatedArg = { annotation = """({ contains : Maybe String } -> { contains : Maybe String })""", arg = "fillInOptionals" }
+                , letBindings =
+                    [ """filledInOptionals =
+            fillInOptionals { contains = Nothing }"""
+                    , """optionalArgs =
+    [ Argument.optional "contains" filledInOptionals.contains Encode.string ]
+        |> List.filterMap identity
+"""
+                    ]
+                }
+
+
+generateSingle : Type.Arg -> Maybe Int
+generateSingle { name, typeRef } =
+    case typeRef of
+        Type.TypeReference referrableType Type.NonNullable ->
+            Nothing
+
+        Type.TypeReference referrableType Type.Nullable ->
+            Just 123
--- tests/Generator/OptionalArgsTests.elm
+++ tests/Generator/OptionalArgsTests.elm
@@ -2,15 +2,60 @@ module Generator.OptionalArgsTests exposing (all)
 
 import Expect
 import Graphqelm.Generator.OptionalArgs as OptionalArgs
+import Graphqelm.Parser.Scalar as Scalar
+import Graphqelm.Parser.Type as Type
 import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
     describe "optional args generator"
-        [ test "no optional args" <|
+        [ test "no args" <|
             \() ->
                 []
                     |> OptionalArgs.generate
                     |> Expect.equal Nothing
+        , test "no optional args, only required" <|
+            \() ->
+                [ { name = "id"
+                  , typeRef = Type.TypeReference (Type.Scalar Scalar.String) Type.NonNullable
+                  }
+                ]
+                    |> OptionalArgs.generate
+                    |> Expect.equal Nothing
+        , test "with an optional arg" <|
+            \() ->
+                [ { name = "id"
+                  , typeRef = Type.TypeReference (Type.Scalar Scalar.String) Type.Nullable
+                  }
+                ]
+                    |> OptionalArgs.generate
+                    |> Expect.equal
+                        (Just
+                            { annotatedArg = { annotation = """({ contains : Maybe String } -> { contains : Maybe String })""", arg = "fillInOptionals" }
+                            , letBindings =
+                                [ """filledInOptionals =
+            fillInOptionals { contains = Nothing }"""
+                                , """optionalArgs =
+    [ Argument.optional "contains" filledInOptionals.contains Encode.string ]
+        |> List.filterMap identity
+"""
+                                ]
+                            }
+                        )
         ]
+
+
+
+-- menuItems : ({ contains : Maybe String } -> { contains : Maybe String }) -> Object menuItems Api.Object.MenuItem.Type -> Field.Query (List menuItems)
+-- menuItems fillInOptionals object =
+--     let
+--         filledInOptionals =
+--             fillInOptionals { contains = Nothing }
+--
+--         optionalArgs =
+--             [ Argument.optional "contains" filledInOptionals.contains Encode.string ]
+--                 |> List.filterMap identity
+--     in
+--     Object.listOf "menuItems" optionalArgs object
+--         |> Query.rootQuery
