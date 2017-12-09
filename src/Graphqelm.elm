65c5b75837e81eb9defccaab43cf8f8629d3018c
Dillon Kearns <dillon@dillonkearns.com>
2017-12-09 13:16:48 -0800

    Add map and map3 functions.

--- src/Graphqelm.elm
+++ src/Graphqelm.elm
@@ -1,4 +1,4 @@
-module Graphqelm exposing (map2, noOptionalArgs)
+module Graphqelm exposing (map, map2, map3, noOptionalArgs)
 
 import Graphqelm.Field
 import Graphqelm.Object as Object exposing (Object)
@@ -7,6 +7,16 @@ import Graphqelm.Object as Object exposing (Object)
 noOptionalArgs : a -> a
 noOptionalArgs =
     identity
+
+
+map :
+    (placeholder -> Object (a -> result) typeLock)
+    -> placeholder
+    -> Graphqelm.Field.FieldDecoder a typeLock
+    -> Object result typeLock
+map build buildTo a =
+    build buildTo
+        |> Object.with a
 
 
 map2 :
@@ -19,3 +29,17 @@ map2 build buildTo a b =
     build buildTo
         |> Object.with a
         |> Object.with b
+
+
+map3 :
+    (placeholder -> Object (a -> b -> c -> result) typeLock)
+    -> placeholder
+    -> Graphqelm.Field.FieldDecoder a typeLock
+    -> Graphqelm.Field.FieldDecoder b typeLock
+    -> Graphqelm.Field.FieldDecoder c typeLock
+    -> Object result typeLock
+map3 build buildTo a b c =
+    build buildTo
+        |> Object.with a
+        |> Object.with b
+        |> Object.with c
