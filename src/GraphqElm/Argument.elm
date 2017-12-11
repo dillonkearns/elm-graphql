cc1c5530f818023acec5029e896747ec331cfa19
Dillon Kearns <dillon@dillonkearns.com>
2017-12-11 14:13:54 -0800

    Remove unused function.

--- src/GraphqElm/Argument.elm
+++ src/GraphqElm/Argument.elm
@@ -1,4 +1,4 @@
-module Graphqelm.Argument exposing (Argument, enum, int, optional, optionalEnum, string, toQueryString)
+module Graphqelm.Argument exposing (Argument, enum, int, optional, string, toQueryString)
 
 import Graphqelm.Value as Value exposing (Value)
 import Json.Encode
@@ -12,11 +12,6 @@ optional : String -> Maybe a -> (a -> Value) -> Maybe Argument
 optional fieldName maybeValue toValue =
     maybeValue
         |> Maybe.map (\value -> Argument fieldName (toValue value))
-
-
-optionalEnum : String -> Maybe a -> Maybe Argument
-optionalEnum fieldName maybeValue =
-    maybeValue |> Maybe.map (\value -> Argument fieldName (Value.EnumValue (toString value)))
 
 
 argument : String -> Value -> Argument
