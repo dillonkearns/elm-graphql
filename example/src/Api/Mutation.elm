7dcc08ffd5e8d28caaf1af242492aef254fde80d
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 10:51:18 -0800

    Update imports in menu example.

--- example/src/Api/Mutation.elm
+++ example/src/Api/Mutation.elm
@@ -3,13 +3,11 @@ module Api.Mutation exposing (..)
 import Api.Object
 import Graphqelm exposing (RootMutation)
 import Graphqelm.Builder.Argument as Argument exposing (Argument)
-import Graphqelm.Field as Field exposing (Field)
 import Graphqelm.FieldDecoder as FieldDecoder exposing (FieldDecoder)
 import Graphqelm.Object as Object exposing (Object)
 import Graphqelm.RootObject as RootObject
 import Graphqelm.Value as Value exposing (Value)
 import Json.Decode as Decode exposing (Decoder)
-import Json.Encode as Encode
 
 
 build : (a -> constructor) -> Object (a -> constructor) RootMutation
