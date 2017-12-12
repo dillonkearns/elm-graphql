39e8bb173e12935a6ceca6fd625acd3325795f97
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 13:13:53 -0800

    Remove unnecessary helper functions.

--- src/Graphqelm.elm
+++ src/Graphqelm.elm
@@ -1,11 +1,6 @@
-module Graphqelm exposing (RootMutation, RootQuery, map, map2, map3)
+module Graphqelm exposing (RootMutation, RootQuery)
 
 {-|
-
-
-## Alternative Map syntax
-
-@docs map, map2, map3
 
 
 ## Top-Level Types
@@ -13,9 +8,6 @@ module Graphqelm exposing (RootMutation, RootQuery, map, map2, map3)
 @docs RootMutation, RootQuery
 
 -}
-
-import Graphqelm.FieldDecoder exposing (FieldDecoder)
-import Graphqelm.Object as Object exposing (Object)
 
 
 {-| Type for top-level queries which can be sent using functions
@@ -30,45 +22,3 @@ from `Graphqelm.Http`.
 -}
 type RootMutation
     = RootMutation
-
-
-{-| TODO
--}
-map :
-    (placeholder -> Object (a -> result) typeLock)
-    -> placeholder
-    -> FieldDecoder a typeLock
-    -> Object result typeLock
-map build buildTo a =
-    build buildTo
-        |> Object.with a
-
-
-{-| TODO
--}
-map2 :
-    (placeholder -> Object (a -> b -> result) typeLock)
-    -> placeholder
-    -> FieldDecoder a typeLock
-    -> FieldDecoder b typeLock
-    -> Object result typeLock
-map2 build buildTo a b =
-    build buildTo
-        |> Object.with a
-        |> Object.with b
-
-
-{-| TODO
--}
-map3 :
-    (placeholder -> Object (a -> b -> c -> result) typeLock)
-    -> placeholder
-    -> FieldDecoder a typeLock
-    -> FieldDecoder b typeLock
-    -> FieldDecoder c typeLock
-    -> Object result typeLock
-map3 build buildTo a b c =
-    build buildTo
-        |> Object.with a
-        |> Object.with b
-        |> Object.with c
