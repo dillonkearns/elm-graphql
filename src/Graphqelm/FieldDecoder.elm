c522de38347f71e93fd9cf56f52bfa70ad10491e
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 10:52:51 -0800

    Remove unused function.

--- src/Graphqelm/FieldDecoder.elm
+++ src/Graphqelm/FieldDecoder.elm
@@ -1,18 +1,11 @@
-module Graphqelm.FieldDecoder exposing (FieldDecoder(FieldDecoder), fieldDecoder, map)
+module Graphqelm.FieldDecoder exposing (FieldDecoder(FieldDecoder), map)
 
-import Graphqelm.Builder.Argument as Argument exposing (Argument)
 import Graphqelm.Field as Field exposing (Field)
 import Json.Decode as Decode exposing (Decoder)
 
 
 type FieldDecoder decodesTo typeLock
     = FieldDecoder Field (Decoder decodesTo)
-
-
-fieldDecoder : String -> List Argument -> Decoder decodesTo -> FieldDecoder decodesTo lockedTo
-fieldDecoder fieldName args decoder =
-    FieldDecoder (Field.Leaf fieldName args)
-        (decoder |> Decode.field fieldName)
 
 
 map : (decodesTo -> mapsTo) -> FieldDecoder decodesTo typeLock -> FieldDecoder mapsTo typeLock
