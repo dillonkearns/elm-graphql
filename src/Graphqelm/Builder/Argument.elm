3118266b9553c824f376798887bc5c92f2d17152
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 13:03:32 -0800

    Add docs for Builder.Argument.

--- src/Graphqelm/Builder/Argument.elm
+++ src/Graphqelm/Builder/Argument.elm
@@ -1,12 +1,20 @@
 module Graphqelm.Builder.Argument exposing (Argument(Argument), enum, int, optional, string)
 
+{-| Internal for auto-generated code from the `graphqelm` CLI.
+@docs Argument, enum, int, optional, string
+-}
+
 import Graphqelm.Encode as Encode exposing (Value)
 
 
+{-| Argument type.
+-}
 type Argument
     = Argument String Value
 
 
+{-| Used for passing optional arguments in generated code.
+-}
 optional : String -> Maybe a -> (a -> Value) -> Maybe Argument
 optional fieldName maybeValue toValue =
     maybeValue
@@ -18,16 +26,22 @@ argument fieldName value =
     Argument fieldName value
 
 
+{-| String argument.
+-}
 string : String -> String -> Argument
 string fieldName value =
     argument fieldName (Encode.string value)
 
 
+{-| Enum argument.
+-}
 enum : String -> String -> Argument
 enum fieldName value =
     argument fieldName (Encode.enum toString value)
 
 
+{-| Int argument.
+-}
 int : String -> Int -> Argument
 int fieldName value =
     argument fieldName (Encode.int value)
