f82fc20471ac599dbfebf31c89d30eae04431370
Dillon Kearns <dillon@dillonkearns.com>
2017-12-12 13:06:32 -0800

    Add docs for Buidler.Object.

--- src/Graphqelm/Builder/Object.elm
+++ src/Graphqelm/Builder/Object.elm
@@ -1,4 +1,8 @@
-module Graphqelm.Builder.Object exposing (..)
+module Graphqelm.Builder.Object exposing (fieldDecoder, listOf, object, single)
+
+{-| Internal functions for use by auto-generated code from the `graphqelm` CLI.
+@docs fieldDecoder, listOf, object, single
+-}
 
 import Graphqelm.Builder.Argument exposing (Argument)
 import Graphqelm.Field as Field exposing (Field)
@@ -7,22 +11,30 @@ import Graphqelm.Object exposing (Object(Object))
 import Json.Decode as Decode exposing (Decoder)
 
 
+{-| Refer to a field in auto-generated code.
+-}
 fieldDecoder : String -> List Argument -> Decoder decodesTo -> FieldDecoder decodesTo lockedTo
 fieldDecoder fieldName args decoder =
     FieldDecoder (Field.Leaf fieldName args)
         (decoder |> Decode.field fieldName)
 
 
+{-| Refer to list of objects in auto-generated code.
+-}
 listOf : String -> List Argument -> Object a objectTypeLock -> FieldDecoder (List a) lockedTo
 listOf fieldName args (Object fields decoder) =
     FieldDecoder (Field.Composite fieldName args fields) (Decode.list decoder |> Decode.field fieldName)
 
 
+{-| Refer to single object in auto-generated code.
+-}
 single : String -> List Argument -> Object a objectTypeLock -> FieldDecoder a lockedTo
 single fieldName args (Object fields decoder) =
     FieldDecoder (Field.Composite fieldName args fields) (decoder |> Decode.field fieldName)
 
 
+{-| Used to create the `build` functions in auto-generated code.
+-}
 object : (a -> constructor) -> Object (a -> constructor) typeLock
 object constructor =
     Object [] (Decode.succeed constructor)
