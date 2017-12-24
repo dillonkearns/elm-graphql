c220eca9ee89394cb1cb1486abf8667cf76b237d
Dillon Kearns <dillon@dillonkearns.com>
2017-12-24 14:26:44 -0800

    Add test boilerplate for Document.

---
+++ tests/DocumentTests.elm
@@ -0,0 +1,27 @@
+module DocumentTests exposing (all)
+
+import Expect
+import Graphqelm
+import Graphqelm.Document
+import Graphqelm.Field exposing (Field(Leaf))
+import Graphqelm.SelectionSet exposing (SelectionSet(SelectionSet))
+import Json.Decode as Decode
+import Test exposing (Test, describe, test)
+
+
+document : List Field -> SelectionSet () Graphqelm.RootQuery
+document fields =
+    SelectionSet fields (Decode.fail "")
+
+
+all : Test
+all =
+    describe "document"
+        [ test "single leaf" <|
+            \() ->
+                document [ Leaf "viewer" [] ]
+                    |> Graphqelm.Document.serializeQuery
+                    |> Expect.equal """query {
+  result1: viewer
+}"""
+        ]
