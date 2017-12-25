4ad7d889d95c5ff83140cd36c6252d48b0c7bbf8
Dillon Kearns <dillon@dillonkearns.com>
2017-12-25 15:13:34 -0800

    Add encode tests.

---
+++ tests/EncodeTests.elm
@@ -0,0 +1,16 @@
+module EncodeTests exposing (all)
+
+import Expect
+import Graphqelm.Encode
+import Test exposing (Test, describe, test)
+
+
+all : Test
+all =
+    describe "encode"
+        [ test "string" <|
+            \() ->
+                Graphqelm.Encode.string "hello"
+                    |> Graphqelm.Encode.serialize
+                    |> Expect.equal "\"hello\""
+        ]
