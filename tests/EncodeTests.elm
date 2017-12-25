4d781de784e72bb3b60bedd575c74eb966d0ab00
Dillon Kearns <dillon@dillonkearns.com>
2017-12-25 15:23:56 -0800

    Add Encode.object.

--- tests/EncodeTests.elm
+++ tests/EncodeTests.elm
@@ -5,6 +5,11 @@ import Graphqelm.Encode
 import Test exposing (Test, describe, test)
 
 
+(=>) : a -> b -> ( a, b )
+(=>) =
+    (,)
+
+
 all : Test
 all =
     describe "encode"
@@ -23,4 +28,10 @@ all =
                 Graphqelm.Encode.int 123
                     |> Graphqelm.Encode.serialize
                     |> Expect.equal "123"
+        , test "empty object" <|
+            \() ->
+                Graphqelm.Encode.object
+                    []
+                    |> Graphqelm.Encode.serialize
+                    |> Expect.equal """{}"""
         ]
