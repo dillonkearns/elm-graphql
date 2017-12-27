096bf195e795ef39894356b619a0882f6a70b0cb
Dillon Kearns <dillon@dillonkearns.com>
2017-12-27 11:56:32 -0800

    Parse location data in error response.

--- src/Graphqelm/Parser/Response.elm
+++ src/Graphqelm/Parser/Response.elm
@@ -6,14 +6,30 @@ import Json.Decode as Decode exposing (Decoder)
 
 type alias Error =
     { message : String
+    , locations : List Location
     , details : Dict String Decode.Value
     }
 
 
+type alias Location =
+    { line : Int, column : Int }
+
+
+locationDecoder : Decode.Decoder Location
+locationDecoder =
+    Decode.map2 Location
+        (Decode.field "line" Decode.int)
+        (Decode.field "column" Decode.int)
+
+
 errorDecoder : Decoder (List Error)
 errorDecoder =
-    Decode.map2 Error
+    Decode.map3 Error
         (Decode.field "message" Decode.string)
-        (Decode.dict Decode.value |> Decode.map (Dict.remove "message"))
+        (Decode.field "locations" (Decode.list locationDecoder))
+        (Decode.dict Decode.value
+            |> Decode.map (Dict.remove "message")
+            |> Decode.map (Dict.remove "locations")
+        )
         |> Decode.list
         |> Decode.field "errors"
--- tests/ResponseTests.elm
+++ tests/ResponseTests.elm
@@ -7,18 +7,6 @@ import Json.Decode
 import Test exposing (Test, describe, test)
 
 
-location : Json.Decode.Value
-location =
-    case
-        Json.Decode.decodeString Json.Decode.value """[{"line":4,"column":5}]"""
-    of
-        Ok value ->
-            value
-
-        Err error ->
-            Debug.crash ("Couldn't parse json value\n" ++ error)
-
-
 all : Test
 all =
     describe "parser"
@@ -26,13 +14,11 @@ all =
             \() ->
                 """{"data":null,"errors":[{"message":"You must provide a `first` or `last` value to properly paginate the `releases` connection.","locations":[{"line":4,"column":5}]}]}"""
                     |> Json.Decode.decodeString Graphqelm.Parser.Response.errorDecoder
-                    |> Debug.log "result"
                     |> Expect.equal
                         (Ok
                             [ { message = "You must provide a `first` or `last` value to properly paginate the `releases` connection."
-                              , details =
-                                    Dict.fromList
-                                        [ ( "locations", location ) ]
+                              , locations = [ { line = 4, column = 5 } ]
+                              , details = Dict.empty
                               }
                             ]
                         )
