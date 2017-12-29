8d5e146e64091c5e82eaeb9b6d2a8c37ba51da43
Dillon Kearns <dillon@dillonkearns.com>
2017-12-29 12:22:57 -0800

    Generate hardcoded arg doc.

--- src/Graphqelm/Generator/DocComment.elm
+++ src/Graphqelm/Generator/DocComment.elm
@@ -5,13 +5,28 @@ import Interpolate exposing (interpolate)
 
 
 generate : Field -> String
-generate { description } =
+generate { description, args } =
     case description of
         Just actualDescription ->
-            interpolate """{-| {0}
+            interpolate """{-| {0}{1}
 -}
 """
-                [ actualDescription ]
+                [ actualDescription, argsDoc args ]
 
         Nothing ->
             ""
+
+
+argsDoc : List Type.Arg -> String
+argsDoc args =
+    case args of
+        [] ->
+            ""
+
+        _ ->
+            "\n\n  - id - The human's id.\n"
+
+
+argDoc : Type.Arg -> String
+argDoc { description } =
+    ""
--- tests/Generator/DocCommentTests.elm
+++ tests/Generator/DocCommentTests.elm
@@ -21,4 +21,23 @@ all =
                     |> Expect.equal """{-| A human in the star wars universe.
 -}
 """
+        , test "field with arg" <|
+            \() ->
+                { name = "human"
+                , description = Just "A human in the star wars universe."
+                , typeRef = Type.TypeReference (Type.InterfaceRef "Human") Type.NonNullable
+                , args =
+                    [ { name = "id"
+                      , description = Just "The human's id."
+                      , typeRef = Type.TypeReference (Type.Scalar Scalar.String) Type.NonNullable
+                      }
+                    ]
+                }
+                    |> DocComment.generate
+                    |> Expect.equal """{-| A human in the star wars universe.
+
+  - id - The human's id.
+
+-}
+"""
         ]
