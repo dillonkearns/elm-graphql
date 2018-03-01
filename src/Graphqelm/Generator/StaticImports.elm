7454098c4b2e6d5c370e2309363714d89e722c18
Dillon Kearns <dillon@dillonkearns.com>
2018-03-01 15:00:15 -0800

    Extract static imports list to module. Use in query module.

---
+++ src/Graphqelm/Generator/StaticImports.elm
@@ -0,0 +1,29 @@
+module Graphqelm.Generator.StaticImports exposing (all)
+
+import Graphqelm.Generator.Context exposing (Context)
+import Interpolate exposing (interpolate)
+
+
+all : Context -> String
+all { apiSubmodule } =
+    interpolate
+        """import Graphqelm.Internal.Builder.Argument as Argument exposing (Argument)
+import Graphqelm.Field as Field exposing (Field)
+import Graphqelm.Internal.Builder.Object as Object
+import Graphqelm.Internal.Encode as Encode exposing (Value)
+import Graphqelm.Operation exposing (RootMutation, RootQuery, RootSubscription)
+import Graphqelm.OptionalArgument exposing (OptionalArgument(Absent))
+import Graphqelm.SelectionSet exposing (SelectionSet)
+import Json.Decode as Decode exposing (Decoder)
+import {0}.Object
+import {0}.Interface
+import {0}.Union
+import {0}.Scalar
+import {0}.InputObject
+import Graphqelm.Internal.Builder.Object as Object
+import Graphqelm.OptionalArgument exposing (OptionalArgument(Absent))
+import Graphqelm.SelectionSet exposing (SelectionSet)
+import Graphqelm.Operation exposing (RootMutation, RootQuery, RootSubscription)
+import Json.Decode as Decode exposing (Decoder)
+import Graphqelm.Internal.Encode as Encode exposing (Value)"""
+        [ apiSubmodule |> String.join "." ]
