870ee2f21ba336b63b4ec8986ef8115be1a414b2
Dillon Kearns <dillon@dillonkearns.com>
2018-03-13 15:23:28 -0700

    Add AnnotatedArg helper module.

---
+++ tests/Generator/AnnotatedArgTests.elm
@@ -0,0 +1,38 @@
+module Generator.AnnotatedArgTests exposing (all)
+
+import Expect
+import Graphqelm.Generator.AnnotatedArg as AnnotatedArg
+import Test exposing (Test, describe, test)
+
+
+all : Test
+all =
+    describe "annotated args"
+        [ test "return value only" <|
+            \() ->
+                AnnotatedArg.build "String"
+                    |> AnnotatedArg.toString "foo"
+                    |> Expect.equal
+                        """foo : String
+foo  =
+"""
+        , test "with arg" <|
+            \() ->
+                AnnotatedArg.build "String"
+                    |> AnnotatedArg.prepend ( "Int", "n" )
+                    |> AnnotatedArg.toString "intToString"
+                    |> Expect.equal
+                        """intToString : Int -> String
+intToString n =
+"""
+        , test "with two args" <|
+            \() ->
+                AnnotatedArg.build "String"
+                    |> AnnotatedArg.prepend ( "Int", "n" )
+                    |> AnnotatedArg.prepend ( "(String -> String)", "mapFn" )
+                    |> AnnotatedArg.toString "mapInt"
+                    |> Expect.equal
+                        """mapInt : (String -> String) -> Int -> String
+mapInt mapFn n =
+"""
+        ]
