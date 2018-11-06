1a22ad048003eac9002be32c63f4da6dfd677721
Dillon Kearns <dillon@dillonkearns.com>
2018-11-06 11:25:00 -0800

    Apply new formatting rules.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -1,59 +1,59 @@
-import { spawn } from 'child_process'
-import * as fs from 'fs-extra'
+import { spawn } from "child_process";
+import * as fs from "fs-extra";
 
-const globalInstallPath = `${__dirname}/../node_modules/.bin/elm-format`
-const localInstallPath = `${__dirname}/../../.bin/elm-format`
+const globalInstallPath = `${__dirname}/../node_modules/.bin/elm-format`;
+const localInstallPath = `${__dirname}/../../.bin/elm-format`;
 
 export const writeFile = (path: string, value: string): void => {
-  const elmFormatPath = findElmFormatPath()
+  const elmFormatPath = findElmFormatPath();
 
   if (elmFormatPath) {
-    writeWithElmFormat(path, value, elmFormatPath)
+    writeWithElmFormat(path, value, elmFormatPath);
   } else {
-    writeWithoutFormatting(path, value)
+    writeWithoutFormatting(path, value);
   }
-}
+};
 
 const writeWithElmFormat = (
   path: string,
   value: string,
   elmFormatPath: string
 ): void => {
-  const elmFormat = spawn(elmFormatPath, ['--stdin', '--output', path], {
+  const elmFormat = spawn(elmFormatPath, ["--stdin", "--output", path], {
     shell: true
-  })
+  });
 
-  elmFormat.stdin.write(value)
-  elmFormat.stdin.end()
+  elmFormat.stdin.write(value);
+  elmFormat.stdin.end();
 
-  elmFormat.stdout.on('data', data => {
-    console.log(data.toString())
-  })
+  elmFormat.stdout.on("data", data => {
+    console.log(data.toString());
+  });
 
-  elmFormat.stderr.on('data', data => {
-    console.log(data.toString())
-  })
+  elmFormat.stderr.on("data", data => {
+    console.log(data.toString());
+  });
 
-  elmFormat.on('close', code => {
+  elmFormat.on("close", code => {
     if (code !== 0) {
       console.log(`elm-format process exited with code ${code}.
 Was attempting to write to path ${path} with contents:
-${value}`)
-      process.exit(code)
+${value}`);
+      process.exit(code);
     }
-  })
-}
+  });
+};
 
 const writeWithoutFormatting = (path: string, value: string) => {
-  fs.writeFileSync(path, value)
-}
+  fs.writeFileSync(path, value);
+};
 
 const findElmFormatPath = (): string | null => {
   if (fs.existsSync(globalInstallPath)) {
-    return globalInstallPath
+    return globalInstallPath;
   } else if (fs.existsSync(localInstallPath)) {
-    return localInstallPath
+    return localInstallPath;
   } else {
-    return null
+    return null;
   }
-}
+};
