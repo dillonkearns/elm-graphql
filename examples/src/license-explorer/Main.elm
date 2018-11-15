5b358d9f835124dcc42fb31ff4e62449054e881e
Dillon Kearns <dillon@dillonkearns.com>
2018-11-15 14:39:32 -0800

    Update license explorer example.

--- examples/src/license-explorer/Main.elm
+++ examples/src/license-explorer/Main.elm
@@ -8,13 +8,14 @@ import Element.Events
 import ElmPackage
 import Github.Object
 import Github.Object.License
+import Github.Object.LicenseRule
 import Github.Query
 import Github.Scalar
 import Graphql.Document as Document
 import Graphql.Field as Field exposing (Field)
 import Graphql.Http
 import Graphql.Operation exposing (RootQuery)
-import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, fieldSelection, with)
+import Graphql.SelectionSet as SelectionSet exposing (SelectionSet, fieldSelection, with, withFragment)
 import Html exposing (Html, a, button, div, h1, img, p, pre, text)
 import Html.Attributes exposing (href, src, style, target)
 import Html.Events exposing (onClick)
@@ -25,12 +26,16 @@ import RepoWithOwner
 import View.Result
 
 
+type alias FullLicense =
+    { overview : LicenseOverview
+    , details : LicenseDetails
+    }
+
+
 type alias LicenseOverview =
     { name : String
     , key : String
-
-    -- , nickname : Maybe String
-    -- , url : Maybe Github.Scalar.Uri
+    , url : Maybe Github.Scalar.Uri
     }
 
 
@@ -39,11 +44,26 @@ fragmentLicenseOverview =
     Github.Object.License.selection LicenseOverview
         |> with Github.Object.License.name
         |> with Github.Object.License.key
+        |> with Github.Object.License.url
 
 
+type alias LicenseDetails =
+    { conditions : List String
+    }
 
--- |> with Github.Object.License.nickname
--- |> with Github.Object.License.url
+
+fragmentLicenseDetails : SelectionSet LicenseDetails Github.Object.License
+fragmentLicenseDetails =
+    Github.Object.License.selection LicenseDetails
+        |> with
+            (Github.Object.License.conditions (fieldSelection Github.Object.LicenseRule.label)
+                |> removeNothingsFromList
+            )
+
+
+removeNothingsFromList : Field (List (Maybe a)) typeLock -> Field (List a) typeLock
+removeNothingsFromList =
+    Field.map (List.filterMap identity)
 
 
 makeRequest =
@@ -54,11 +74,15 @@ makeRequest =
 
 
 type Msg
-    = GotLicenses (RemoteData (Graphql.Http.Error ()) (List LicenseOverview))
+    = GotLicenses LicenseResponse
+
+
+type alias LicenseResponse =
+    RemoteData (Graphql.Http.Error ()) (List FullLicense)
 
 
 type alias Model =
-    { licenses : RemoteData (Graphql.Http.Error ()) (List LicenseOverview)
+    { licenses : LicenseResponse
     }
 
 
@@ -74,8 +98,15 @@ init _ =
     )
 
 
+fullLicenseSelection =
+    Github.Object.License.selection FullLicense
+        |> withFragment fragmentLicenseOverview
+        |> withFragment fragmentLicenseDetails
+
+
+query : SelectionSet (List FullLicense) RootQuery
 query =
-    Github.Query.licenses fragmentLicenseOverview
+    Github.Query.licenses fullLicenseSelection
         |> Field.map (List.filterMap identity)
         |> fieldSelection
 
@@ -102,11 +133,12 @@ view model =
         |> Element.layout []
 
 
-licenseView : LicenseOverview -> Element Msg
-licenseView licenseOverview =
+licenseView : FullLicense -> Element Msg
+licenseView licenseDetails =
     Element.row [ Element.spaceEvenly, Element.width Element.fill ]
-        [ Element.text licenseOverview.name
-        , Element.text licenseOverview.key
+        [ Element.text licenseDetails.overview.name
+        , Element.text licenseDetails.overview.key
+        , licenseDetails.details.conditions |> Debug.toString |> Element.text
         ]
 
 
