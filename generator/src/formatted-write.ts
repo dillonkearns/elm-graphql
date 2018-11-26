dbea233a77baa24dff5d2b3d09328b0d3af337fa
Dillon Kearns <dillon@dillonkearns.com>
2018-11-26 14:59:09 -0800

    Add peer dependency and use npx to run elm-format.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -5,24 +5,5 @@ const globalInstallPath = `${__dirname}/../node_modules/.bin/elm-format`;
 const localInstallPath = `${__dirname}/../../.bin/elm-format`;
 
 export const applyElmFormat = (fileOrFolderToFormat: string): void => {
-  const elmFormatPath = findElmFormatPath();
-
-  if (!elmFormatPath) {
-    console.log("Applying elm-format...");
-    execSync(
-      elmFormatPath + " --elm-version=0.19 --yes " + fileOrFolderToFormat + "/"
-    );
-  } else {
-    console.log("Couldn't find elm-format binary, skipping formatting...");
-  }
-};
-
-const findElmFormatPath = (): string | null => {
-  if (fs.existsSync(globalInstallPath)) {
-    return globalInstallPath;
-  } else if (fs.existsSync(localInstallPath)) {
-    return localInstallPath;
-  } else {
-    return null;
-  }
+  execSync(`npx elm-format --elm-version=0.19 --yes ${fileOrFolderToFormat}/`);
 };
