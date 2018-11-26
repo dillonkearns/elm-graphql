fa9db95e1ede7b1774cf97f376017c589bafcc36
Dillon Kearns <dillon@dillonkearns.com>
2018-11-26 13:59:55 -0800

    Apply prettier formatting.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -4,21 +4,16 @@ import * as fs from "fs-extra";
 const globalInstallPath = `${__dirname}/../node_modules/.bin/elm-format`;
 const localInstallPath = `${__dirname}/../../.bin/elm-format`;
 
-
-export const applyElmFormat = (
-  fileOrFolderToFormat: string,
-): void => {
+export const applyElmFormat = (fileOrFolderToFormat: string): void => {
   const elmFormatPath = findElmFormatPath();
-  
+
   // If there isn't elm-format, nothing to do
-  if(!elmFormatPath) 
-     return;
-  
-  console.log("Applying elm-format...")
+  if (!elmFormatPath) return;
+
+  console.log("Applying elm-format...");
   const elmFormat = execSync(
     elmFormatPath + " --elm-version=0.19 --yes " + fileOrFolderToFormat + "/"
   );
-    
 };
 
 const findElmFormatPath = (): string | null => {
