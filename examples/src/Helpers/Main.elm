1dd2734a73cd8417534d12bd17f082ee5ad83749
Dillon Kearns <dillon@dillonkearns.com>
2018-12-07 07:18:15 -0800

    Wrap helper document function with sub msgs.

--- examples/src/Helpers/Main.elm
+++ examples/src/Helpers/Main.elm
@@ -1,27 +1,59 @@
-module Helpers.Main exposing (document, view)
+module Helpers.Main exposing (Msg, document, view)
 
 import Browser
-import Html exposing (div, h1, p, pre, text)
+import DateFormat exposing (text)
+import Html exposing (div, h1, input, label, p, pre, text)
+import Html.Attributes exposing (type_)
+import Html.Events exposing (onClick)
 import PrintAny
 import Regex
 
 
+type Msg subMsg
+    = ToggleAliases
+    | SubMsg subMsg
+
+
+document :
+    { init : flags -> ( model, Cmd subMsg )
+    , update : subMsg -> model -> ( model, Cmd subMsg )
+    , queryString : String
+    }
+    -> Program flags model (Msg subMsg)
 document { init, update, queryString } =
     Browser.document
-        { init = init
-        , update = update
+        { init = mapInit init
+        , update = mapUpdate update
         , subscriptions = \_ -> Sub.none
         , view = view queryString
         }
 
 
-view : String -> a -> Browser.Document msg
+mapInit : (flags -> ( subModel, Cmd subMsg )) -> flags -> ( subModel, Cmd (Msg subMsg) )
+mapInit subInit flags =
+    subInit flags
+        |> Tuple.mapSecond (Cmd.map SubMsg)
+
+
+mapUpdate : (subMsg -> subModel -> ( subModel, Cmd subMsg )) -> Msg subMsg -> subModel -> ( subModel, Cmd (Msg subMsg) )
+mapUpdate subUpdate msg subModel =
+    case msg of
+        ToggleAliases ->
+            ( subModel, Cmd.none )
+
+        SubMsg subMsg ->
+            subUpdate subMsg subModel
+                |> Tuple.mapSecond (Cmd.map SubMsg)
+
+
+view : String -> a -> Browser.Document (Msg subMsg)
 view query model =
     { title = "Query Explorer"
     , body =
         [ div []
             [ div []
                 [ h1 [] [ text "Generated Query" ]
+                , p [] [ toggleAliasesCheckbox ]
                 , pre []
                     [ query
                         |> stripAliases
@@ -35,6 +67,13 @@ view query model =
             ]
         ]
     }
+
+
+toggleAliasesCheckbox =
+    label []
+        [ input [ type_ "checkbox", onClick ToggleAliases ] []
+        , text "Toggle Aliases"
+        ]
 
 
 stripAliases query =
