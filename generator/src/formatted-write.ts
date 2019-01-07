75e5caef026eeded2b7fa6ba39568f76da43913a
Dillon Kearns <dillon@dillonkearns.com>
2019-01-07 13:28:17 -0800

    Remove unused variables.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -1,9 +1,6 @@
 import { execSync } from "child_process";
 import * as fs from "fs-extra";
 
-const globalInstallPath = `${__dirname}/../node_modules/.bin/elm-format`;
-const localInstallPath = `${__dirname}/../../.bin/elm-format`;
-
 export const applyElmFormat = (fileOrFolderToFormat: string): void => {
   execSync(`npx elm-format --elm-version=0.19 --yes ${fileOrFolderToFormat}/`);
 };
