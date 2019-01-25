a054c355e8ffd9b8fec26d6f5a93a8ef15e9c990
Dillon Kearns <dillon@dillonkearns.com>
2019-01-25 09:29:51 -0800

    Add @docs line.

--- src/Graphql/Codec.elm
+++ src/Graphql/Codec.elm
@@ -3,9 +3,10 @@ module Graphql.Codec exposing (Codec)
 {-| This module is used when you define custom scalars codecs for your schema.
 See an example and steps for how to set this up in your codebase here:
 <https://github.com/dillonkearns/elm-graphql/blob/master/examples/src/Example07CustomCodecs.elm>
--}
 
--- import Graphql.Internal.Encode as Encode exposing (Value)
+@docs Codec
+
+-}
 
 import Json.Decode
 import Json.Encode
