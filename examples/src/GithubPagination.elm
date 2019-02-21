ad8895b13b000873a918fe6eccba36fa4e65b572
Dillon Kearns <dillon@dillonkearns.com>
2019-02-21 18:29:52 +0100

    Use paginationArguments helper.

--- examples/src/GithubPagination.elm
+++ examples/src/GithubPagination.elm
@@ -37,30 +37,36 @@ type alias Paginator dataType cursorType =
 -- first query -> No Cursor, just count (first or last)
 -- Result comes back -> Get a cursor, maintain the count
 -- second query, Cursor, no count (use from previous)
+-- type PaginationContinuation cursor
+--     = Forward { first : Int } { after : cursor }
+--     | Backward { last : Int } { before : cursor }
 
 
-type PaginationContinuation cursor
-    = Forward { first : Int } { after : cursor }
-    | Backward { last : Int } { before : cursor }
+type PaginationSetup
+    = Forward { first : Int }
+    | Backward { last : Int }
 
 
-paginationArguments : Maybe String -> Query.SearchOptionalArguments -> Query.SearchOptionalArguments
-paginationArguments maybeCursor optionals =
-    { optionals
-        | first = Present 1
-        , after = OptionalArgument.fromMaybe maybeCursor
-    }
+paginationArguments : Maybe String -> PaginationSetup -> Query.SearchOptionalArguments -> Query.SearchOptionalArguments
+paginationArguments maybeCursor paginationSetup optionals =
+    case paginationSetup of
+        Forward { first } ->
+            { optionals
+                | first = Present first
+                , after = OptionalArgument.fromMaybe maybeCursor
+            }
+
+        Backward { last } ->
+            { optionals
+                | last = Present last
+                , before = OptionalArgument.fromMaybe maybeCursor
+            }
 
 
 query : Maybe String -> SelectionSet Response RootQuery
 query cursor =
     Query.search
-        (\optionals ->
-            { optionals
-                | first = Present 1
-                , after = OptionalArgument.fromMaybe cursor
-            }
-        )
+        (paginationArguments cursor (Forward { first = 1 }))
         { query = "language:Elm"
         , type_ = Github.Enum.SearchType.Repository
         }
