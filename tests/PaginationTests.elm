bc600f9d3167f472ae31f7bbaeabe40ccd659f52
Dillon Kearns <dillon@dillonkearns.com>
2019-02-25 18:26:22 -0800

    Add test case for forward pagination.

--- tests/PaginationTests.elm
+++ tests/PaginationTests.elm
@@ -15,27 +15,37 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "pagination"
-        [ test "reverse pagination items are added in correct order" <|
-            \() ->
-                pageOfRequests "3rd" "2nd" "1st"
-                    |> Decode.decodeString
-                        (Paginator.selectionSet 3 Paginator.backward edgesSelection |> Graphql.Document.decoder)
-                    |> expectPaginator (ExpectStillLoading [ "1st", "2nd", "3rd" ])
-        , test "reverse pagination items are added in correct order after second request" <|
-            \() ->
-                pageOfRequests "3rd" "2nd" "1st"
-                    |> Decode.decodeString (Paginator.selectionSet 3 Paginator.backward edgesSelection |> Graphql.Document.decoder)
-                    |> Result.andThen
-                        (\paginator ->
-                            pageOfRequests "6" "5" "4"
-                                |> Decode.decodeString (Paginator.selectionSet 3 paginator edgesSelection |> Graphql.Document.decoder)
-                        )
-                    |> expectPaginator (ExpectStillLoading [ "1st", "2nd", "3rd", "4", "5", "6" ])
+        [ describe "backward"
+            [ test "reverse pagination items are added in correct order" <|
+                \() ->
+                    backwardPageOfRequests "3rd" "2nd" "1st"
+                        |> Decode.decodeString
+                            (Paginator.selectionSet 3 Paginator.backward edgesSelection |> Graphql.Document.decoder)
+                        |> expectPaginator (ExpectStillLoading [ "1st", "2nd", "3rd" ])
+            , test "reverse pagination items are added in correct order after second request" <|
+                \() ->
+                    backwardPageOfRequests "3rd" "2nd" "1st"
+                        |> Decode.decodeString (Paginator.selectionSet 3 Paginator.backward edgesSelection |> Graphql.Document.decoder)
+                        |> Result.andThen
+                            (\paginator ->
+                                backwardPageOfRequests "6" "5" "4"
+                                    |> Decode.decodeString (Paginator.selectionSet 3 paginator edgesSelection |> Graphql.Document.decoder)
+                            )
+                        |> expectPaginator (ExpectStillLoading [ "1st", "2nd", "3rd", "4", "5", "6" ])
+            ]
+        , describe "forward"
+            [ test "paginated items are added in correct order" <|
+                \() ->
+                    forwardPageOfRequests "3rd" "2nd" "1st"
+                        |> Decode.decodeString
+                            (Paginator.selectionSet 3 Paginator.forward edgesSelection |> Graphql.Document.decoder)
+                        |> expectPaginator (ExpectDoneLoading [ "1st", "2nd", "3rd" ])
+            ]
         ]
 
 
-pageOfRequests : String -> String -> String -> String
-pageOfRequests item1 item2 item3 =
+backwardPageOfRequests : String -> String -> String -> String
+backwardPageOfRequests item1 item2 item3 =
     interpolate
         """
                         {
@@ -70,6 +80,45 @@ pageOfRequests item1 item2 item3 =
         [ item1, item2, item3 ]
 
 
+forwardPageOfRequests : String -> String -> String -> String
+forwardPageOfRequests item1 item2 item3 =
+    interpolate
+        """
+        {
+  "data": {
+        "edges": [
+          {
+            "cursor": "Y3Vyc29yOnYyOpIAzglq1vk=",
+            "node": {
+              "login3832528868": "{0}"
+            },
+            "starredAt987198633": "2019-02-24T21:49:48Z"
+          },
+          {
+            "cursor": "Y3Vyc29yOnYyOpIAzglq7e0=",
+            "node": {
+              "login3832528868": "{1}"
+            },
+            "starredAt987198633": "2019-02-24T23:30:53Z"
+          },
+          {
+            "cursor": "Y3Vyc29yOnYyOpIAzglq-Mo=",
+            "node": {
+              "login3832528868": "{2}"
+            },
+            "starredAt987198633": "2019-02-25T00:26:03Z"
+          }
+        ],
+        "pageInfo": {
+          "endCursor12867311": "Y3Vyc29yOnYyOpIAzglq-Mo=",
+          "hasNextPage3880003826": false
+        }
+  }
+}
+                        """
+        [ item1, item2, item3 ]
+
+
 edgesSelection =
     Object.selectionForCompositeField "edges" [] loginFieldTotal (identity >> Decode.list)
 
@@ -84,10 +133,7 @@ loginField =
 
 type PaginationExpection node
     = ExpectStillLoading (List node)
-
-
-
--- | ExpectDoneLoading (List node)
+    | ExpectDoneLoading (List node)
 
 
 expectPaginator : PaginationExpection node -> Result error (Paginator direction node) -> Expect.Expectation
@@ -101,6 +147,20 @@ expectPaginator expectation result =
                     }
                         |> Expect.equal
                             { moreToLoad = True
+                            , nodes = expectedNodes
+                            }
+
+                Err error ->
+                    Expect.fail (Debug.toString error)
+
+        ExpectDoneLoading expectedNodes ->
+            case result of
+                Ok paginator ->
+                    { moreToLoad = paginator |> Paginator.moreToLoad
+                    , nodes = paginator |> Paginator.nodes
+                    }
+                        |> Expect.equal
+                            { moreToLoad = False
                             , nodes = expectedNodes
                             }
 
