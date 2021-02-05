e12e576b40a875f20bf97e1b2f1412f1f9e6e714
Dillon Kearns <dillon@dillonkearns.com>
2021-02-05 08:58:01 -0800

    Use cross-spawn to run elm-format.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -1,8 +1,8 @@
-import { execSync } from "child_process";
+import * as spawn from "cross-spawn";
 
 export const applyElmFormat = (fileOrFolderToFormat: string): void => {
   try {
-    execSync(`elm-format --elm-version=0.19 --yes ${fileOrFolderToFormat}`);
+    const result = spawn.sync(`elm-format`, ['--elm-version', '0.19', '--yes', fileOrFolderToFormat]);
   } catch (error) {
     console.error("Unable to run elm-format. Please ensure that elm-format is available on your PATH and try again. Or re-run using --skip-elm-format.\n\n", error);
     process.exit(1);
