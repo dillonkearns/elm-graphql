b6d91319f33387c7974aa6f3197a68bc0a32fadc
Dillon Kearns <dillon@dillonkearns.com>
2021-02-05 09:18:22 -0800

    Check for non-zero status to check for success.

--- generator/src/formatted-write.ts
+++ generator/src/formatted-write.ts
@@ -3,13 +3,12 @@ import * as spawn from "cross-spawn";
 export const applyElmFormat = (fileOrFolderToFormat: string): void => {
   try {
     const result = spawn.sync(`elm-format`, ['--elm-version', '0.19', '--yes', fileOrFolderToFormat]);
-    const error = result.error
-    if (error) {
-      if (error.code === "ENOENT") {
+    if (result.status !== 0) {
+      if (result?.error?.code === "ENOENT") {
         console.error("Unable to run elm-format. Please ensure that elm-format is available on your PATH and try again. Or re-run using --skip-elm-format.");
         process.exit(1);
       } else {
-        console.error("Unable to run elm-format. Please ensure that elm-format is available on your PATH and try again. Or re-run using --skip-elm-format.\n\n", error);
+        console.error("Unable to run elm-format. Please ensure that elm-format is available on your PATH and try again. Or re-run using --skip-elm-format.\n\n", result.error);
         process.exit(1);
       }
     }
