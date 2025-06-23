7b7bdc21bcd5e6021e8fd6223f640f97792087c2
Dillon Kearns <dillon@incrementalelm.com>
2025-06-23 07:10:50 -0700

    Update versions file.

--- generator/src/versions.js
+++ generator/src/versions.js
@@ -1,4 +1,4 @@
 // This file is auto-generated during build
 // Do not modify manually
-export const npmPackageVersion = "4.3.2-beta.0";
+export const npmPackageVersion = "4.3.2";
 export const elmPackageVersion = "5.0.12";
